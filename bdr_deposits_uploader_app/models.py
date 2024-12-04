9ace05b6888bee388a62005231afb7be10452e75
Birkin James Diana <birkin.diana@gmail.com>
2024-12-04 10:00:00 -0500

    updates admin and documentation.

--- bdr_deposits_uploader_app/models.py
+++ bdr_deposits_uploader_app/models.py
@@ -3,9 +3,16 @@ from django.db import models
 
 
 class UserProfile(models.Model):
+    """
+    This extends the User object to include additional fields.
+
+    This webapp is set up to create a UserProfile record automatically when a User record is created.
+    See the README for more info about that.
+    """
+
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    extra_field_a = models.CharField(max_length=20, unique=True)
-    extra_field_b = models.CharField(max_length=20, unique=True)
+    extra_field_a = models.CharField(max_length=20, unique=False, null=True, blank=True)
+    extra_field_b = models.CharField(max_length=20, unique=False, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username}'s profile"
