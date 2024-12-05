70247568b537205a208f248c7c3555eb7825d7a6
Birkin James Diana <birkin.diana@gmail.com>
2024-12-05 17:27:59 -0500

    adds upload-slug url to router.

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('info/', views.info, name='info_url'),
     path('config/new/', views.config_new, name='config_new_url'),
     path('config/<str:slug>/', views.config_slug, name='config_slug_url'),
+    path('upload/<str:slug>/', views.upload_slug, name='upload_slug_url'),
     ## other --------------------------------------------------------
     path('', views.root, name='root_url'),
     path('admin/', admin.site.urls),
