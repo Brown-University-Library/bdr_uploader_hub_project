7ee9e2c6dd468f105acf36cb37268cf47bd7b05d
Birkin James Diana <birkin.diana@gmail.com>
2024-12-09 09:50:54 -0500

    adds type-hints to signals.py

--- bdr_deposits_uploader_app/signals.py
+++ bdr_deposits_uploader_app/signals.py
@@ -4,26 +4,24 @@ Enables auto-creation of a UserProfile record when a new User record is created.
 See the README for more info.
 """
 
+import logging
+from typing import Any, Type
+
 from django.conf import settings
+from django.db.models import Model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from bdr_deposits_uploader_app.models import UserProfile
 
-# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-# def create_or_update_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         UserProfile.objects.create(user=instance)
-#     else:
-#         UserProfile.objects.get_or_create(user=instance)
-#         instance.userprofile.save()
+log = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
+def create_or_update_user_profile(sender: Type[Model], instance: Model, created: bool, **kwargs: Any) -> None:
     if created:
-        # Check if a UserProfile already exists to avoid duplication
-        UserProfile.objects.get_or_create(user=instance)
+        log.debug('created was True, so getting or creating a UserProfile record')
+        UserProfile.objects.get_or_create(user=instance)  # check if a UserProfile already exists to avoid duplication-error
     else:
-        # Update or save an existing UserProfile
-        instance.userprofile.save()
+        log.debug('created was False, so updating the existing UserProfile record')
+        instance.userprofile.save()  # update or save existing UserProfile
