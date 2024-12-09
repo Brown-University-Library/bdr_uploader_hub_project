c30a523ff9ad8aae7121beb8ec3f7a7ee3e6f417
Birkin James Diana <birkin.diana@gmail.com>
2024-12-09 09:57:33 -0500

    adds login-url

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from bdr_deposits_uploader_app import views
 urlpatterns = [
     ## main ---------------------------------------------------------
     path('info/', views.info, name='info_url'),
+    path('login/', views.login, name='login_url'),
     path('config/new/', views.config_new, name='config_new_url'),
     path('config/<str:slug>/', views.config_slug, name='config_slug_url'),
     path('upload/<str:slug>/', views.upload_slug, name='upload_slug_url'),
