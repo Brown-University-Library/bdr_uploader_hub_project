fa033518ac5759e76cde4d0f36bdd1766423e4b4
Birkin James Diana <birkin.diana@gmail.com>
2024-12-11 09:56:36 -0500

    removes inline admin feature.

--- bdr_deposits_uploader_app/admin.py
+++ bdr_deposits_uploader_app/admin.py
@@ -1,25 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
 
 from .models import UserProfile
 
+# No custom UserAdmin registration, so remove:
+# admin.site.unregister(User)
+# admin.site.register(User, CustomUserAdmin)
 
-## enables the user-profile model's fields to be edited inline with the user model (they're at the bottom)
-class UserProfileInline(admin.StackedInline):
-    model = UserProfile
-    can_delete = False
-    verbose_name_plural = 'User Profile'
-
-
-## custom User admin, incorporating UserProfileInline above
-class CustomUserAdmin(UserAdmin):
-    inlines = (UserProfileInline,)
-
-
-## re-register UserAdmin
-admin.site.unregister(User)
-admin.site.register(User, CustomUserAdmin)
-
-## register UserProfile separately -- in case we want to edit it directly
+# Just register UserProfile normally
 admin.site.register(UserProfile)
