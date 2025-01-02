f444ff4671c4e86f31806a35549bafcd79cdcad7
Birkin James Diana <birkin.diana@gmail.com>
2025-01-02 09:01:34 -0500

    removes inline-script-metadata.

--- run_tests.py
+++ run_tests.py
@@ -1,16 +1,3 @@
-## <https://peps.python.org/pep-0723/> recently adopted python standard.
-## Allows, for development, to run the app via `uv run ./manage.py runserver`, with no venv.
-# /// script
-# requires-python = "~=3.12.0"
-# dependencies = [
-#     "django~=4.2.0",
-#     "python-dotenv~=1.0.0",
-#     "requests~=2.27.0",
-#     "trio~=0.26.0",
-#     "urllib3~=1.26.0",
-# ]
-# ///
-
 import os
 import sys
 import tempfile
