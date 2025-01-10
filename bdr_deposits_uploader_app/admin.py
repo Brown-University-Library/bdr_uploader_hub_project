b55b779383929a2050852152c26fddf6f883d483
Birkin James Diana <birkin.diana@gmail.com>
2025-01-10 10:16:14 -0500

    adds submission admin.

--- bdr_deposits_uploader_app/admin.py
+++ bdr_deposits_uploader_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import AppConfig, UserProfile
+from .models import AppConfig, Submission, UserProfile
 
 ## for django-auth
 admin.site.register(UserProfile)
 
 ## other models
 admin.site.register(AppConfig)
+admin.site.register(Submission)
