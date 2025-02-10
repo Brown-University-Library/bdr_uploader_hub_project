e4d0a3c06f5f60f9ebeea201841a85b85ff19c34
Birkin James Diana <birkin.diana@gmail.com>
2025-02-10 17:32:37 -0500

    implements initial dynamic student-upload-form class.

---
+++ bdr_deposits_uploader_app/forms/student_upload_form.py
@@ -0,0 +1,116 @@
+from django import forms
+
+
+def get_student_upload_form_class(config_data: dict) -> type[forms.Form]:
+    """
+    Dynamically creates and returns a StudentUploadForm class based on the staff-config form data.
+    """
+    fields = {}
+
+    ## Collaborators section ----------------------------------------
+    if config_data.get('offer_advisors_and_readers'):
+        fields['advisors_and_readers'] = forms.CharField(
+            label='Advisors and Readers',
+            required=config_data.get('advisors_and_readers_required', False),
+            help_text='Enter names or identifiers for advisors/readers',
+        )
+    if config_data.get('offer_team_members'):
+        fields['team_members'] = forms.CharField(
+            label='Team Members',
+            required=config_data.get('team_members_required', False),
+            help_text='Enter names or identifiers for team members',
+        )
+    if config_data.get('offer_faculty_mentors'):
+        fields['faculty_mentors'] = forms.CharField(
+            label='Faculty Mentors',
+            required=config_data.get('faculty_mentors_required', False),
+            help_text='Enter names or identifiers for faculty mentors',
+        )
+    if config_data.get('offer_authors'):
+        fields['authors'] = forms.CharField(
+            label='Authors', required=config_data.get('authors_required', False), help_text='Enter author names'
+        )
+
+    ## Department & Research Program section ------------------------
+    if config_data.get('offer_department'):
+        fields['department_options'] = forms.MultipleChoiceField(
+            choices=[('dept1', 'Department 1'), ('dept2', 'Department 2')],
+            widget=forms.CheckboxSelectMultiple,
+            label='Department Options',
+            required=config_data.get('department_required', False),
+            help_text='Select one or more departments',
+        )
+    if config_data.get('offer_research_program'):
+        fields['research_program_options'] = forms.MultipleChoiceField(
+            choices=[('prog1', 'Program 1'), ('prog2', 'Program 2')],
+            widget=forms.CheckboxSelectMultiple,
+            label='Research Program Options',
+            required=config_data.get('research_program_required', False),
+            help_text='Select one or more research programs',
+        )
+
+    ## access section ------------------------------------------------
+    if config_data.get('offer_license_options'):
+        fields['license_options'] = forms.MultipleChoiceField(
+            choices=[('license1', 'License 1'), ('license2', 'License 2')],
+            label='License Options',
+            required=config_data.get('license_required', False),
+            help_text='Select at least one license',
+        )
+        fields['license_default'] = forms.CharField(
+            label='License Default',
+            required=config_data.get('license_required', False),
+            help_text='Enter the default license',
+        )
+    if config_data.get('offer_access_options'):
+        fields['access_options'] = forms.MultipleChoiceField(
+            choices=[('access1', 'Access 1'), ('access2', 'Access 2')],
+            label='Access Options',
+            required=config_data.get('access_required', False),
+            help_text='Select at least one access option',
+        )
+        fields['access_default'] = forms.CharField(
+            label='Access Default',
+            required=config_data.get('access_required', False),
+            help_text='Enter the default access option',
+        )
+    if config_data.get('offer_visibility_options'):
+        fields['visibility_options'] = forms.MultipleChoiceField(
+            choices=[('vis1', 'Visibility 1'), ('vis2', 'Visibility 2')],
+            label='Visibility Options',
+            required=config_data.get('visibility_required', False),
+            help_text='Select at least one visibility option',
+        )
+        fields['visibility_default'] = forms.CharField(
+            label='Visibility Default',
+            required=config_data.get('visibility_required', False),
+            help_text='Enter the default visibility',
+        )
+
+    ## other section -------------------------------------------------
+    if config_data.get('ask_for_concentrations'):
+        fields['concentrations'] = forms.CharField(
+            label='Concentrations',
+            required=config_data.get('concentrations_required', False),
+            help_text='Enter concentration information',
+        )
+    if config_data.get('ask_for_degrees'):
+        fields['degrees'] = forms.CharField(
+            label='Degrees', required=config_data.get('degrees_required', False), help_text='Enter degree information'
+        )
+    if config_data.get('invite_supplementary_files'):
+        fields['supplementary_files'] = forms.FileField(
+            label='Supplementary Files', required=False, help_text='Upload supplementary files if needed'
+        )
+
+    # Always include these even if they might be blank in the staff config.
+    fields['authorized_student_groups'] = forms.CharField(
+        label='Authorized Student Groups', required=False, widget=forms.Textarea, help_text='Example: "group:A | group:B"'
+    )
+    fields['authorized_student_emails'] = forms.CharField(
+        label='Authorized Student Emails', required=False, widget=forms.Textarea, help_text='Example: "email1 | email2"'
+    )
+
+    # Dynamically create a new Form class.
+    StudentUploadForm = type('StudentUploadForm', (forms.Form,), fields)
+    return StudentUploadForm
