075d3790aff76681c394197185a9ed3a362250dc
Birkin James Diana <birkin.diana@gmail.com>
2025-02-28 14:29:53 -0500

    adds initial status-choices.

--- bdr_deposits_uploader_app/models.py
+++ bdr_deposits_uploader_app/models.py
@@ -43,40 +43,57 @@ class Submission(models.Model):
     This model represents a user's submission of a deposit.
     """
 
+    STATUS_CHOICES = (
+        ('not_submitted', 'Not Submitted'),
+        ('waiting_for_review', 'Awaiting Review'),
+        ('approved', 'Approved'),
+        ('ingested', 'Ingested'),  # fully ingested, including supplementary files
+        ('ingest_error', 'Ingestion Error'),
+    )
+
+    ## non-form-data ------------------------------------------------
+
+    ## general ----------------------------------
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    ## basics -------------------------------------------------------
     app = models.ForeignKey(AppConfig, on_delete=models.CASCADE, null=True)
+
+    ## timestamps -------------------------------
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    ## form-data ----------------------------------------------------
+
+    ## basics -----------------------------------
     student_eppn = models.CharField(max_length=255, blank=True, null=True)
     student_email = models.CharField(max_length=255, blank=True, null=True)
     title = models.CharField(max_length=255)
     abstract = models.TextField()
-    ## collaborators ------------------------------------------------
+    ## collaborators ----------------------------
     advisors_and_readers = models.CharField(max_length=255, blank=True, null=True)
     team_members = models.CharField(max_length=255, blank=True, null=True)
     faculty_mentors = models.CharField(max_length=255, blank=True, null=True)
     authors = models.CharField(max_length=255, blank=True, null=True)
-    ## departments/programs -----------------------------------------
+    ## departments/programs ---------------------
     department = models.CharField(max_length=255, blank=True, null=True)
     research_program = models.CharField(max_length=255, blank=True, null=True)
-    ## access and visibility -----------------------------------------
+    ## access and visibility --------------------
     license_options = models.CharField(max_length=100, blank=True, null=True)
     visibility_options = models.CharField(max_length=100, blank=True, null=True)
-    ## other --------------------------------------------------------
+    ## other ------------------------------------
     concentrations = models.CharField(max_length=255, blank=True, null=True)
     degrees = models.CharField(max_length=255, blank=True, null=True)
-    ## file stuff ---------------------------------------------------
+    ## file stuff -------------------------------
     primary_file = models.FileField(upload_to='primary_files/', blank=True, null=True)
     supplementary_files = models.FileField(upload_to='supplementary_files/', blank=True, null=True)
     original_file_name = models.CharField(max_length=255, blank=True, null=True)
     staged_file_name = models.CharField(max_length=255, blank=True, null=True)  # added field
     checksum_type = models.CharField(max_length=100, blank=True, null=True)
     checksum = models.CharField(max_length=255, blank=True, null=True)
-    ## form-data ----------------------------------------------------
+    ## form-data --------------------------------
     temp_submission_json = models.JSONField(default=dict, blank=True)
-    ## timestamps ---------------------------------------------------
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         title_short = self.title if len(self.title) <= 10 else f'{self.title[:10]}...'
         return title_short
+
+    ## end class Submission()
