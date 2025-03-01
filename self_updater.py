38567086631d5deb51cdfdfe19d97a2ce2fbc31e
Birkin James Diana <birkin.diana@gmail.com>
2025-03-01 06:58:50 -0500

    explores clearer patch-compatibility-notation.

--- self_updater.py
+++
@@ -1,8 +0,0 @@
-"""
-Enables automatic self-updating! (To a limited extent.)
-
-This script has been replaced by:
-<https://github.com/Brown-University-Library/self_updater_code>
-
-...which can be installed on a server and used to update multiple projects.
-"""
