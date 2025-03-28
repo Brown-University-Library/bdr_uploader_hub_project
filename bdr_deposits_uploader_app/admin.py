8d7517e571b9f9d406e17a9a4100068adf032b08
Birkin James Diana <birkin.diana@gmail.com>
2025-03-28 17:02:23 -0400

    stubs an action.

--- bdr_deposits_uploader_app/admin.py
+++ bdr_deposits_uploader_app/admin.py
@@ -1,6 +1,11 @@
-from django.contrib import admin
+import logging
+
+from django.contrib import admin, messages
 
 from .models import AppConfig, Submission, UserProfile
+
+log = logging.getLogger(__name__)
+
 
 ## for django-auth --------------------------------------------------
 admin.site.register(UserProfile)
@@ -14,6 +19,18 @@ class SubmissionAdmin(admin.ModelAdmin):
     list_filter = ('app', 'created_at', 'updated_at')
     search_fields = ('title', 'app', 'created_at', 'updated_at')
     ordering = ('-created_at',)
+
+    actions = ['ingest']
+
+    def ingest(self, request, queryset):
+        log.debug('ingest-action called')
+        log.debug(f'queryset: ``{queryset}``')
+        messages.info(request, 'Theses approved')
+        for submission in queryset:
+            log.debug(f'processing submission: ``{submission}``')
+        return
+
+    ingest.short_description = 'Ingest selected submissions'
 
 
 admin.site.register(AppConfig)
