3874cbdc6d82de99720e356c650d2022f871f277
Birkin James Diana <birkin.diana@gmail.com>
2025-04-13 09:05:47 -0400

    validates selections on attempted ingest.

--- bdr_deposits_uploader_app/admin.py
+++ bdr_deposits_uploader_app/admin.py
@@ -43,9 +43,21 @@ class SubmissionAdmin(admin.ModelAdmin):
     def ingest(self, request, queryset):
         log.debug('ingest-action called')
         log.debug(f'queryset: ``{queryset}``')
-        messages.info(request, 'Theses approved')
+        # messages.info(request, 'Theses approved')
+        ## confirm that all selections are ready to ingest ----------
+        errors: list = []
         for submission in queryset:
-            log.debug(f'processing submission: ``{submission}``')
+            log.debug(f'processing submission.title: ``{submission.title}``')
+            log.debug(f'submission.status: ``{submission.status}``')
+            if not submission.status == 'ready_to_ingest':
+                # errors.append(str(submission))
+                errors.append(f'`{str(submission.id)[0:4]}...--{str(submission)}`')
+                # messages.warning(request, f'Submission ``{submission.title}`` not ready to ingest')
+            if errors:
+                messages.warning(request, f'Invalid selections: {", ".join(errors)}')
+                return
+        ## ingest the selected submissions --------------------------
+        messages.success(request, 'Submissions ingested')
         return
 
     ingest.short_description = 'Ingest selected submissions'
