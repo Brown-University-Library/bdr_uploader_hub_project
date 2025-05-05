1cba25d77fad0f1fff105bda12f40e65937d420d
Birkin James Diana <birkin.diana@gmail.com>
2025-05-05 17:31:45 -0400

    implements initial mods-rendering via template.

--- bdr_deposits_uploader_app/lib/mods_handler.py
+++ bdr_deposits_uploader_app/lib/mods_handler.py
@@ -1,4 +1,7 @@
 import logging
+
+from django.template.loader import get_template
+
 from bdr_deposits_uploader_app.models import Submission
 
 log = logging.getLogger(__name__)
@@ -13,3 +16,14 @@ class ModsMaker:
         Manages the creation of the mods xml file.
         """
         log.debug('prepare_mods called')
+        title = self.submission.title
+        abstract = self.submission.abstract
+        context = {
+            'title': title,
+            'abstract': abstract,
+        }
+        ## render the template
+        template = get_template('mods_base.xml')
+        mods_xml: str = template.render(context)
+        log.debug(f'mods_xml: ``{mods_xml}``')
+        return mods_xml
