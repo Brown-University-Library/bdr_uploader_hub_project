ae3730f65306ecdcc98b6a44cae49b0888806095
Birkin James Diana <birkin.diana@gmail.com>
2025-05-05 17:30:53 -0400

    updates name.

--- bdr_deposits_uploader_app/admin.py
+++ bdr_deposits_uploader_app/admin.py
@@ -68,61 +68,6 @@ class SubmissionAdmin(admin.ModelAdmin):
 
     ingest.short_description = 'Ingest selected submissions'
 
-    # ## ingest action ------------------------------------------------
-    # def ingest(self, request, queryset):
-    #     log.debug('ingest-action called')
-    #     log.debug(f'queryset: ``{queryset}``')
-    #     ## confirm that all selections are ready to ingest ----------
-    #     errors: list = []
-    #     for submission in queryset:
-    #         log.debug(f'processing submission.title: ``{submission.title}``')
-    #         log.debug(f'submission.status: ``{submission.status}``')
-    #         if not submission.status == 'ready_to_ingest':
-    #             errors.append(f'`{str(submission.id)[0:4]}...--{str(submission)}`')
-    #             log.warning(
-    #                 f'`{str(submission.id)[0:4]}...--{str(submission)}` not ready to ingest; status: {submission.status}'
-    #             )
-    #         if errors:
-    #             messages.warning(request, f'Invalid selections: {", ".join(errors)}')
-    #             return
-    #     ## ingest the selected submissions --------------------------
-    #     for submission in queryset:
-    #         """
-    #         attempt to ingest
-    #         - on failure:
-    #             - log problem
-    #             - change status to `ingest_error`
-    #             - save bdr-pid if I have it
-    #             - save
-    #         - on success:
-    #             - change status to `ingested`
-    #             - save bdr-pid
-    #             - save
-    #         """
-    #         ## happy path, for logic --------------------------------
-    #         ingester = Ingester(submission)
-    #         ingester.prepare_mods()
-    #         ingester.prepare_rights()
-    #         ingester.prepare_ir()
-    #         ingester.prepare_rels()
-    #         ingester.prepare_file()
-    #         ingester.parameterize()
-    #         result: tuple[str | None, str | None] = ingester.post()
-    #         (pid, error_message) = result
-    #         log.info(f'ingested submission: {submission}')
-    #         submission.bdr_pid = pid
-    #         submission.status = 'ingested'
-    #         submission.ingest_error_message = None
-    #         submission.save()
-    #         send_ingest_success_email(
-    #             submission.first_name, submission.email, submission.title, submission.bdr_url
-    #         )  # bdr_url will be a property based on bdr_pid
-    #         log.info('sent_ingestion_confirmation email')
-    #     messages.success(request, 'Submissions ingested')
-    #     return
-
-    # ingest.short_description = 'Ingest selected submissions'
-
 
 admin.site.register(AppConfig)
 admin.site.register(Submission, SubmissionAdmin)
