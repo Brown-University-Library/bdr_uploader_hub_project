eddba6c217c135500c87ca3248eb3bdec8e57a57
Birkin James Diana <birkin.diana@gmail.com>
2025-05-08 15:51:37 -0400

    refactors basic test.

--- bdr_deposits_uploader_app/tests/test_mods_maker.py
+++ bdr_deposits_uploader_app/tests/test_mods_maker.py
@@ -10,7 +10,85 @@ from bdr_deposits_uploader_app.models import Submission
 log = logging.getLogger(__name__)
 
 
-class ModsMakerTest(SimpleTestCase):
+class ModsMakerBasicTest(SimpleTestCase):
+    """
+    Tests the mods_maker function with minimal submitted info.
+
+    To add a test:
+    - create a submission-instance
+    - pass it to ModsMaker(submission).prepare_mods()
+    - assert the expected output.
+    """
+
+    def setUp(self):
+        """
+        Set up the test case.
+        """
+        self.submission = Submission(
+            title='foo foo',
+            abstract='bar bar',
+            created_at=datetime.datetime(2025, 5, 6, 18, 26, 37),
+        )
+        self.result: str = ModsMaker(self.submission).prepare_mods()
+        log.debug(f'mods_maker result: ``{self.result}``')
+
+    def test_prepare_mods_basic(self):
+        """
+        Tests the mods_maker function with minimal submitted info.
+
+        This test mostly confirms the test-structure is valid.
+
+        TODO: This submission-object would never be passed to the mods because it's missing required fields.
+              Revise to use a valid minimal-info submission-object.
+        """
+        self.assertIn('<mods:title>foo foo</mods:title>', self.result)
+        self.assertIn('<mods:abstract>bar bar</mods:abstract>', self.result)
+        self.assertIn('<mods:dateCreated keyDate="yes" encoding="w3cdtf">2025</mods:dateCreated>', self.result)
+        self.assertIn('<mods:dateIssued encoding="w3cdtf">2025-05-06</mods:dateIssued>', self.result)
+        self.assert_standard_mods_elements(self.result)
+
+    def test_assert_standard_mods_elements(self):
+        """
+        Not a direct-test; called from different test functions.
+        Asserts common MODS elements that should be present in all MODS XML outputs.
+        """
+        self.assertIn('<mods:typeOfResource authority="primo">text_resources</mods:typeOfResource>', self.result)
+        self.assertIn(
+            '<mods:genre authority="aat" valueURI="http://vocab.getty.edu/aat/300444670">scholarly works</mods:genre>',
+            self.result,
+        )
+        ## originInfo -----------------------------------------------
+        self.assertIn('<mods:originInfo>', self.result)
+        self.assertIn(
+            '<mods:placeTerm type="code" authority="marccountry" authorityURI="http://www.loc.gov/marc/countries/">riu</mods:placeTerm>',
+            self.result,
+        )
+        self.assertIn(
+            '<mods:placeTerm type="text">Providence, Rhode Island</mods:placeTerm>',
+            self.result,
+        )
+        self.assertIn(
+            '<mods:publisher authority="naf" authorityURI="http://id.loc.gov/authorities/names">Brown University Library</mods:publisher>',
+            self.result,
+        )
+        ## physicalDescription ---------------------------------------
+        self.assertIn('<mods:physicalDescription>', self.result)
+        self.assertIn('<mods:extent>1 document</mods:extent>', self.result)
+        self.assertIn('<mods:digitalOrigin>born digital</mods:digitalOrigin>', self.result)
+        ## accessCondition -------------------------------------------
+        self.assertIn(
+            '<mods:accessCondition type="use and reproduction">All rights reserved</mods:accessCondition>', self.result
+        )
+        self.assertIn(
+            '<mods:accessCondition type="rights statement" xlink:href="http://rightsstatements.org/vocab/InC/1.0/">In Copyright</mods:accessCondition>',
+            self.result,
+        )
+        self.assertIn(
+            '<mods:accessCondition type="restriction on access">All Rights Reserved</mods:accessCondition>', self.result
+        )
+
+
+class ModsMakerFullTest(SimpleTestCase):
     """
     Tests the mods_maker function.
 
