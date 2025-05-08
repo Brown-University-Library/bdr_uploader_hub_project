d894275cecee9c8c7015e4e65f13b0491932333a
Birkin James Diana <birkin.diana@gmail.com>
2025-05-08 16:07:52 -0400

    refactors full-mods test class to test each field separately.

--- bdr_deposits_uploader_app/tests/test_mods_maker.py
+++ bdr_deposits_uploader_app/tests/test_mods_maker.py
@@ -33,10 +33,6 @@ class ModsMakerBasicStaticFieldsTest(SimpleTestCase):
         log.debug(f'mods_maker result: ``{self.result}``')
 
     def test_assert_standard_mods_elements(self):
-        """
-        Not a direct-test; called from different test functions.
-        Asserts common MODS elements that should be present in all MODS XML outputs.
-        """
         self.assertIn('<mods:typeOfResource authority="primo">text_resources</mods:typeOfResource>', self.result)
         self.assertIn(
             '<mods:genre authority="aat" valueURI="http://vocab.getty.edu/aat/300444670">scholarly works</mods:genre>',
@@ -116,12 +112,7 @@ class ModsMakerBasicCustomFieldsTest(SimpleTestCase):
 
 class ModsMakerFullTest(SimpleTestCase):
     """
-    Tests the mods_maker function.
-
-    To add a test:
-    - create a submission-instance
-    - pass it to ModsMaker(submission).prepare_mods()
-    - assert the expected output.
+    Tests the mods_maker function by submitting a full submission-object with fields containing multiple values.
     """
 
     def setUp(self):
@@ -145,21 +136,20 @@ class ModsMakerFullTest(SimpleTestCase):
             created_at=datetime.datetime(2025, 5, 8, 7, 53, 21, 29655),
         )
         self.result: str = ModsMaker(self.submission).prepare_mods()
+        self.soup = BeautifulSoup(self.result, 'xml')
         log.debug(f'mods_maker result: ``{self.result}``')
 
-    def test_prepare_mods_with_full_submission(self):
+    def test_title_generation(self):
         """
-        Tests the mods_maker function with a submission containing all fields.
+        Tests that the title is correctly generated in the MODS XML.
         """
         self.assertIn('<mods:title>2025-may-08 7:50am title</mods:title>', self.result)
 
-        ## Verify fields using BeautifulSoup ------------------------
-        soup = BeautifulSoup(self.result, 'xml')
-        log.debug(f'soup: ``{soup}``')
-
-        ## author check ---------------------------------------------
+    def test_author_generation(self):
         """
-        I want to check for...
+        Tests that authors are correctly generated with proper role attributes.
+
+        Expected XML structure:
         ```xml
         <mods:name type="personal">
             <mods:namePart>auth1first auth1last</mods:namePart>
@@ -167,16 +157,9 @@ class ModsMakerFullTest(SimpleTestCase):
                 <mods:roleTerm authority="marcrelator" authorityURI="http://id.loc.gov/vocabulary/relators" valueURI="http://id.loc.gov/vocabulary/relators/aut">Author</mods:roleTerm>
             </mods:role>
         </mods:name>
-        <mods:name type="personal">
-            <mods:namePart>auth2first auth2last</mods:namePart>
-            <mods:role>
-                <mods:roleTerm authority="marcrelator" authorityURI="http://id.loc.gov/vocabulary/relators" valueURI="http://id.loc.gov/vocabulary/relators/aut">Author</mods:roleTerm>
-            </mods:role>
-        </mods:name>
         ```
         """
-        ## Find all name elements with author role
-        name_elements = soup.find_all('name')
+        name_elements = self.soup.find_all('name')
         author_names = []
         for name in name_elements:
             role_term = name.find(
@@ -184,9 +167,8 @@ class ModsMakerFullTest(SimpleTestCase):
             )
             if role_term:
                 author_names.append(name)
-        log.debug(f'author_names: ``{author_names}``')
         self.assertEqual(len(author_names), 2, 'Should have found 2 author name elements')
-        ## Verify each author name has the correct role structure
+
         for author_name in author_names:
             role = author_name.find('role')
             self.assertIsNotNone(role, 'Author name should have a role element')
@@ -199,48 +181,45 @@ class ModsMakerFullTest(SimpleTestCase):
                 'http://id.loc.gov/vocabulary/relators/aut',
                 'Role term should have correct valueURI',
             )
-        ## Verify that both authors have the correct namePart content
+
         author1_name = author_names[0].find('namePart')
         author2_name = author_names[1].find('namePart')
         self.assertIsNotNone(author1_name, 'First author should have a namePart element')
         self.assertIsNotNone(author2_name, 'Second author should have a namePart element')
         self.assertEqual(author1_name.text, 'auth1first auth1last', 'First author name should match expected text')
         self.assertEqual(author2_name.text, 'auth2first auth2last', 'Second author name should match expected text')
+        ## end def test_author_generation()
 
-        ## advisors/readers check -----------------------------------
+    def test_advisor_reader_generation(self):
         """
-        I want to check for...
+        Tests that advisors and readers are correctly generated with proper role attributes.
+
+        Expected XML structure:
         ```xml
         <mods:name type="personal">
             <mods:namePart>adv-rdr1first adv-rdr1last</mods:namePart>
-            <mods:role>
-                <mods:roleTerm>Advisor/Reader</mods:roleTerm>   
-            </mods:role>
-        </mods:name>
-        <mods:name type="personal">
-            <mods:namePart>adv-rdr2first adv-rdr2last</mods:namePart>
             <mods:role>
                 <mods:roleTerm>Advisor/Reader</mods:roleTerm>
             </mods:role>
         </mods:name>
         ```
         """
-        ## Find all name elements with advisor/reader role
+        name_elements = self.soup.find_all('name')
         advisor_reader_names = []
         for name in name_elements:
             role_term = name.find('roleTerm', text='Advisor/Reader')
             if role_term:
                 advisor_reader_names.append(name)
-        log.debug(f'advisor_reader_names: ``{advisor_reader_names}``')
+
         self.assertEqual(len(advisor_reader_names), 2, 'Should have found 2 advisor/reader name elements')
-        ## Verify each advisor/reader name has the correct role structure
+
         for advisor_reader_name in advisor_reader_names:
             role = advisor_reader_name.find('role')
             self.assertIsNotNone(role, 'Advisor/Reader name should have a role element')
             role_term = role.find('roleTerm')
             self.assertIsNotNone(role_term, 'Role should have a roleTerm element')
             self.assertEqual(role_term.text, 'Advisor/Reader', "Role term text should be 'Advisor/Reader'")
-        ## Verify that each advisor/reader has the correct namePart content
+
         advisor_reader1_name = advisor_reader_names[0].find('namePart')
         advisor_reader2_name = advisor_reader_names[1].find('namePart')
         self.assertIsNotNone(advisor_reader1_name, 'First advisor-reader should have a namePart element')
@@ -251,21 +230,23 @@ class ModsMakerFullTest(SimpleTestCase):
         self.assertEqual(
             advisor_reader2_name.text, 'adv-rdr2first adv-rdr2last', 'Second advisor-reader name should match expected text'
         )
+        ## end def test_advisor_reader_generation()
 
-        ## concentration check --------------------------------------
+    def test_concentration_generation(self):
         """
-        I want to check for...
+        Tests that concentrations are correctly generated as notes with proper displayLabel.
+
+        Expected XML structure:
         ```xml
         <mods:note displayLabel="Scholarly concentration">conc name1</mods:note>
-        <mods:note displayLabel="Scholarly concentration">conc name2</mods:note>        
         ```
         """
-        ## Find all note elements with concentration displayLabel
-        concentration_notes = soup.find_all('note', {'displayLabel': 'Scholarly concentration'})
+        concentration_notes = self.soup.find_all('note', {'displayLabel': 'Scholarly concentration'})
         self.assertEqual(len(concentration_notes), 2, 'Should have found 2 concentration note elements')
-        ## Verify each concentration note has the correct content
-        concentration1_note = concentration_notes[0].find('note')
-        concentration2_note = concentration_notes[1].find('note')
+
+        ## verify each concentration note has the correct content
+        concentration1_note = concentration_notes[0]
+        concentration2_note = concentration_notes[1]
         self.assertEqual(concentration1_note.text, 'conc name1', 'First concentration note should match expected text')
         self.assertEqual(concentration2_note.text, 'conc name2', 'Second concentration note should match expected text')
 
