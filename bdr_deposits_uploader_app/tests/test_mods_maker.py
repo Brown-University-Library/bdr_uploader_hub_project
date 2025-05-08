97da15590a13e97cfbc8aea143fec438357c2b76
Birkin James Diana <birkin.diana@gmail.com>
2025-05-08 15:55:14 -0400

    refactors mods-tests.

--- bdr_deposits_uploader_app/tests/test_mods_maker.py
+++ bdr_deposits_uploader_app/tests/test_mods_maker.py
@@ -10,7 +10,7 @@ from bdr_deposits_uploader_app.models import Submission
 log = logging.getLogger(__name__)
 
 
-class ModsMakerBasicTest(SimpleTestCase):
+class ModsMakerBasicStaticFieldsTest(SimpleTestCase):
     """
     Tests the mods_maker function with minimal submitted info.
 
@@ -31,21 +31,6 @@ class ModsMakerBasicTest(SimpleTestCase):
         )
         self.result: str = ModsMaker(self.submission).prepare_mods()
         log.debug(f'mods_maker result: ``{self.result}``')
-
-    def test_prepare_mods_basic(self):
-        """
-        Tests the mods_maker function with minimal submitted info.
-
-        This test mostly confirms the test-structure is valid.
-
-        TODO: This submission-object would never be passed to the mods because it's missing required fields.
-              Revise to use a valid minimal-info submission-object.
-        """
-        self.assertIn('<mods:title>foo foo</mods:title>', self.result)
-        self.assertIn('<mods:abstract>bar bar</mods:abstract>', self.result)
-        self.assertIn('<mods:dateCreated keyDate="yes" encoding="w3cdtf">2025</mods:dateCreated>', self.result)
-        self.assertIn('<mods:dateIssued encoding="w3cdtf">2025-05-06</mods:dateIssued>', self.result)
-        self.assert_standard_mods_elements(self.result)
 
     def test_assert_standard_mods_elements(self):
         """
@@ -87,6 +72,47 @@ class ModsMakerBasicTest(SimpleTestCase):
             '<mods:accessCondition type="restriction on access">All Rights Reserved</mods:accessCondition>', self.result
         )
 
+    ## end class ModsMakerBasicStaticFieldsTest()
+
+
+class ModsMakerBasicCustomFieldsTest(SimpleTestCase):
+    """
+    Tests the mods_maker function with minimal submitted info.
+
+    To add a test:
+    - create a submission-instance
+    - pass it to ModsMaker(submission).prepare_mods()
+    - assert the expected output.
+    """
+
+    def setUp(self):
+        """
+        Set up the test case.
+        """
+        self.submission = Submission(
+            title='foo foo',
+            abstract='bar bar',
+            created_at=datetime.datetime(2025, 5, 6, 18, 26, 37),
+        )
+        self.result: str = ModsMaker(self.submission).prepare_mods()
+        log.debug(f'mods_maker result: ``{self.result}``')
+
+    def test_prepare_mods_basic(self):
+        """
+        Tests the mods_maker function with minimal submitted info.
+
+        This test mostly confirms the test-structure is valid.
+
+        TODO: This submission-object would never be passed to the mods because it's missing required fields.
+              Revise to use a valid minimal-info submission-object.
+        """
+        self.assertIn('<mods:title>foo foo</mods:title>', self.result)
+        self.assertIn('<mods:abstract>bar bar</mods:abstract>', self.result)
+        self.assertIn('<mods:dateCreated keyDate="yes" encoding="w3cdtf">2025</mods:dateCreated>', self.result)
+        self.assertIn('<mods:dateIssued encoding="w3cdtf">2025-05-06</mods:dateIssued>', self.result)
+
+    ## end class ModsMakerBasicCustomFieldsTest()
+
 
 class ModsMakerFullTest(SimpleTestCase):
     """
@@ -103,66 +129,6 @@ class ModsMakerFullTest(SimpleTestCase):
         Set up the test case.
         """
         pass
-
-    def assert_standard_mods_elements(self, result: str):
-        """
-        Not a direct-test; called from different test functions.
-        Asserts common MODS elements that should be present in all MODS XML outputs.
-        """
-        self.assertIn('<mods:typeOfResource authority="primo">text_resources</mods:typeOfResource>', result)
-        self.assertIn(
-            '<mods:genre authority="aat" valueURI="http://vocab.getty.edu/aat/300444670">scholarly works</mods:genre>',
-            result,
-        )
-        ## originInfo -----------------------------------------------
-        self.assertIn('<mods:originInfo>', result)
-        self.assertIn(
-            '<mods:placeTerm type="code" authority="marccountry" authorityURI="http://www.loc.gov/marc/countries/">riu</mods:placeTerm>',
-            result,
-        )
-        self.assertIn(
-            '<mods:placeTerm type="text">Providence, Rhode Island</mods:placeTerm>',
-            result,
-        )
-        self.assertIn(
-            '<mods:publisher authority="naf" authorityURI="http://id.loc.gov/authorities/names">Brown University Library</mods:publisher>',
-            result,
-        )
-        ## physicalDescription ---------------------------------------
-        self.assertIn('<mods:physicalDescription>', result)
-        self.assertIn('<mods:extent>1 document</mods:extent>', result)
-        self.assertIn('<mods:digitalOrigin>born digital</mods:digitalOrigin>', result)
-        ## accessCondition -------------------------------------------
-        self.assertIn('<mods:accessCondition type="use and reproduction">All rights reserved</mods:accessCondition>', result)
-        self.assertIn(
-            '<mods:accessCondition type="rights statement" xlink:href="http://rightsstatements.org/vocab/InC/1.0/">In Copyright</mods:accessCondition>',
-            result,
-        )
-        self.assertIn(
-            '<mods:accessCondition type="restriction on access">All Rights Reserved</mods:accessCondition>', result
-        )
-
-    def test_prepare_mods_A(self):
-        """
-        Tests the mods_maker function with minimal submitted info.
-
-        This test mostly confirms the test-structure is valid.
-
-        TODO: This submission-object would never be passed to the mods because it's missing required fields.
-              Revise to use a valid minimal-info submission-object.
-        """
-        submission = Submission(
-            title='foo foo',
-            abstract='bar bar',
-            created_at=datetime.datetime(2025, 5, 6, 18, 26, 37),
-        )
-        result: str = ModsMaker(submission).prepare_mods()
-        log.debug(f'mods_maker result: ``{result}``')
-        self.assertIn('<mods:title>foo foo</mods:title>', result)
-        self.assertIn('<mods:abstract>bar bar</mods:abstract>', result)
-        self.assertIn('<mods:dateCreated keyDate="yes" encoding="w3cdtf">2025</mods:dateCreated>', result)
-        self.assertIn('<mods:dateIssued encoding="w3cdtf">2025-05-06</mods:dateIssued>', result)
-        self.assert_standard_mods_elements(result)
 
     def test_prepare_mods_with_full_submission(self):
         """
@@ -305,3 +271,5 @@ class ModsMakerFullTest(SimpleTestCase):
         concentration2_note = concentration_notes[1].find('note')
         self.assertEqual(concentration1_note.text, 'conc name1', 'First concentration note should match expected text')
         self.assertEqual(concentration2_note.text, 'conc name2', 'Second concentration note should match expected text')
+
+    ## end class ModsMakerFullTest()
