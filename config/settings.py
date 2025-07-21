d1dca1ef6f3d46ee2ab7562da1d5d9257e299581
Birkin James Diana <birkin.diana@gmail.com>
2025-07-21 15:53:17 -0400

    updates parentheses.

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ from dotenv import find_dotenv, load_dotenv
 dotenv_path = pathlib.Path(__file__).resolve().parent.parent.parent / '.env'
 # assert dotenv_path.exists(), f'file does not exist, ``{dotenv_path}``'
 # load_dotenv(find_dotenv(str(dotenv_path), raise_error_if_not_found=True), override=True)
-load_dotenv(find_dotenv(str(dotenv_path), override=True))
+load_dotenv(find_dotenv(str(dotenv_path)), override=True)
 
 log = logging.getLogger(__name__)
 
