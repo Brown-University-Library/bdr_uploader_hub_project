e7d0cad7c7a381518bd3e4c15f6e8ec2a3a8e621
Birkin James Diana <birkin.diana@gmail.com>
2025-07-21 15:58:42 -0400

    sets additional dummy envars.

--- run_tests.py
+++ run_tests.py
@@ -9,6 +9,21 @@ from django.test.utils import get_runner
 if __name__ == '__main__':
     os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
     os.environ['SECRET_KEY'] = 'abcd'
+    os.environ['DEBUG_JSON'] = 'true'
+    os.environ['ADMINS_JSON'] = '[]'
+    os.environ['ALLOWED_HOSTS_JSON'] = '[]'
+    os.environ['CSRF_TRUSTED_ORIGINS_JSON'] = '[]'
+    os.environ['DATABASES_JSON'] = '[]'
+    os.environ['STATIC_URL'] = '/static/'
+    os.environ['STATIC_ROOT'] = '/tmp/'
+    os.environ['SERVER_EMAIL'] = 'example@domain.edu'
+    os.environ['EMAIL_HOST'] = 'localhost'
+    os.environ['EMAIL_PORT'] = '1025'
+    os.environ['MEDIA_ROOT'] = '/tmp/'
+    os.environ['BDR_API_FILE_PATH_ROOT'] = '/tmp/'
+    os.environ['FILE_UPLOAD_PERMISSIONS'] = 'None'
+    os.environ['FILE_UPLOAD_DIRECTORY_PERMISSIONS'] = 'None'
+    os.environ['LOG_PATH'] = '/tmp/'
     with tempfile.TemporaryDirectory() as tmp:
         django.setup()
         TestRunner = get_runner(settings)
